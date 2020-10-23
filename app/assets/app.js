6d0cb0d8d4acce4ff6349b65bc791e2f89d6a018
ReBelSuPs <dynosups@outlook.com>
2020-10-23 13:46:04 +0545

    some changes

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,12 +1,1 @@
-const form = document.getElementById('form')
-
-form.onsubmit = (e) => {
-    e.preventDefault();
-    const url = `https://api-test.technofield.net/api/data?token=TEST_TOKEN_123&data=${form.code.value}`;
-    fetch(url).then(res=>console.log(res))
-    form.code.value = '';
-}
-
-Neutralino.init({
-    
-});
+!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){var n=document.getElementById("form");n.onsubmit=function(e){e.preventDefault();var t="https://api-test.technofield.net/api/data?token=TEST_TOKEN_123&data=".concat(n.code.value);fetch(t),n.code.value=""},Neutralino.init({})}]);
